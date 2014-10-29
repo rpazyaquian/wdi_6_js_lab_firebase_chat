cc1544cb3f078fe295d4526d8b42765b1edce287
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-29 15:52:25 -0400

    namespaced everything

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
+// create App
+
+var App = {};
+
 // create firebase reference
+
+
+App.firebase = new Firebase('https://blazing-heat-1056.firebaseio.com/web/data/chat');
+
+App.posts = App.firebase.child('posts');
+
+// 1. add the data
+// 2. get the updated data
+// 3. translate the data, and render/map/process
+
+App.pushMessage = function(message) {
+  App.posts.push({
+    message: message
+  });
+}
+
+App.sendMessage = function(e) {
+  var message = $('#chat-message').val();
+  App.pushMessage(message);
+  $('#chat-message').val('');
+}
+
+App.renderPost = function(post) {
+  // console.log(post.message);
+
+}
 
 $(document).ready(function() {
 
-  var firebaseRef = new Firebase('https://blazing-heat-1056.firebaseio.com/web/data/chat');
-
-  var postsRef = firebaseRef.child('posts');
-
-  // 1. add the data
-  // 2. get the updated data
-  // 3. translate the data, and render/map/process
-
-  var sendMessage = function(e) {
-    var message = $('#chat-message').val();
-    pushMessage(message);
-    $('#chat-message').val('');
-  }
-
-  var pushMessage = function(message) {
-    postsRef.push({
-      message: message
-    });
-  }
-
-  var renderPost = function(post) {
-    // console.log(post.message);
-  }
-
-  $('#chat-submit').click(sendMessage);
+  $('#chat-submit').click(App.sendMessage);
 
   // i want to: render all messages
-  postsRef.on('child_added', function(snapshot) {
+  App.posts.on('child_added', function(snapshot) {
     var post = snapshot.val();
-    console.log(post.message);
-    renderPost(post);
+    App.renderPost(post);
   });
 
 });
